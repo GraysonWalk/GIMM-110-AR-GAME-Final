2b1705b1990830d63349fad81beebee8d1cf92e7
BGarza732 <Brandongarza732@yahoo.com>
2025-12-16 01:34:28 -0700

    update TimebarScript for reboot key also add a trigger

---
+++ Assets/Code/Scripts/StateMachine/RecognigitionState/EmergencyRebootTrigger.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using Vuforia;
+
+public class EmergencyRebootTrigger : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private TimerBar timerBar;
+
+    [Header("Settings")]
+    [SerializeField] private bool singleUse = true;
+
+    private ObserverBehaviour observer;
+    private bool used = false;
+
+    private void Awake()
+    {
+        observer = GetComponent<ObserverBehaviour>();
+    }
+
+    private void OnEnable()
+    {
+        observer.OnTargetStatusChanged += OnTargetStatusChanged;
+    }
+
+    private void OnDisable()
+    {
+        observer.OnTargetStatusChanged -= OnTargetStatusChanged;
+    }
+
+    private void OnTargetStatusChanged(ObserverBehaviour behaviour, TargetStatus status)
+    {
+        if (used && singleUse) return;
+
+        if (status.Status == Status.TRACKED ||
+            status.Status == Status.EXTENDED_TRACKED)
+        {
+            timerBar.RestoreToFull();
+            used = true;
+        }
+    }
+}
+
+
